442daf0e84db72b4145619567715ab6601d0f2dc
GLA Davis <gla.davis89@gmail.com>
2018-05-18 18:30:53 -0700

    moved elements to the if statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,25 +9,29 @@ var input = "";
 document.onkeydown = function(event) {
   var input = event.key;
 
- 
-    var text = possible.charAt(Math.floor(Math.random() * possible.length));
+  var text = possible.charAt(Math.floor(Math.random() * possible.length));
 
-    gsf.push(" " + input);
-    document.getElementById("gl").innerHTML = gl--;
-    document.getElementById("gsf").innerHTML = gsf;
-    document.getElementById("losses").innerHTML = losses;
-    document.getElementById("wins").innerHTML = wins;
-
-  if (text == input) {
-    wins = wins + 1;
-    gl = 9;
-    alert("great jorb homestar!");
-    gsf = [];
-  }
-  if (gl === -1) {
-    losses = losses + 1;
+  gsf.push(" " + input);
+  document.getElementById("gl").innerHTML = gl--;
+  document.getElementById("gsf").innerHTML = gsf;
+  
+  if (gl == -1) {
+    losses++;
     gl = 9;
     gsf = [];
     alert("you fail!");
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("gl").textContent = 9;
+    document.getElementById("gsf").innerHTML = gsf;
+  }
+  
+  if (text == input) {
+    wins++;
+    gl = 9;
+    alert("great jorb homestar!");
+    gsf = [];
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("gl").textContent = 9;
+    document.getElementById("gsf").innerHTML = gsf;
   }
 };
